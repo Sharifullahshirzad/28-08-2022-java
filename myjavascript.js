76eb9ec9c4b33cd81dc28a54c6a4df309cdbbbab
sharifullah shirzad <sharifullahshirzad81@gmail.com>
2022-08-28 11:53:10 +0430

    28-08-2022

---
+++ myjavascript.js
@@ -0,0 +1,31 @@
+
+
+var a , b , c;
+a = 10;
+b =20;
+c = a + b;
+document.write(c);
+
+function showmeheader(){
+document.getElementById("myheader").innerHTML="hi in header from";
+
+}
+
+window.alert("hi i am window alert");
+var x= 5;
+var y= 10;
+var z= x+y;
+console.log(z);
+
+function runblock()
+{
+    document.getElementById('eb1').innerHTML="hello runblack br";
+    document.getElementById('eb2').innerHTML="hello runblack2";
+}
+
+var x = 120;
+var y = 30;
+var z = x + y;
+document.write(z);
+
+
---
+++ myjavascript2.js
@@ -0,0 +1,11 @@
+
+
+let x = "sharifullah";
+let y = "shirzad";
+alert (y)
+
+
+const prod1  = 200;
+const prod2 = 500;
+const prod3 = 500;
+var const = prod1 + prod2 + prod3;
